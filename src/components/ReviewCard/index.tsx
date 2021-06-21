f882c0d4300f814979d872541edf4486b5edefef
Riko <orglary@gmail.com>
2021-06-20 21:16:26 -0300

    feat: Add data to SectionReviews

--- src/components/ReviewCard/index.tsx
+++ src/components/ReviewCard/index.tsx
@@ -1,16 +1,11 @@
 import React, { useEffect } from 'react'
 import ResizeObserver from 'resize-observer-polyfill'
+import { Review } from 'types/api'
+import { getImageUrl } from 'utils/getImageUrl'
 
 import * as S from './styles'
 
-type Props = {
-  id: number
-  name: string
-  image: string
-  description: string
-}
-
-const ReviewCard: React.FC<Props> = ({ id, name, image, description }) => {
+const ReviewCard: React.FC<Review> = ({ name, photo, text }) => {
   useEffect(() => {
     const texts = document.querySelectorAll('p.description')
 
@@ -30,27 +25,13 @@ const ReviewCard: React.FC<Props> = ({ id, name, image, description }) => {
   return (
     <S.Card>
       <S.User>
-        <S.Image>
-          <source
-            srcSet={require(`@images/reviews/${image}?webp`)}
-            type="image/webp"
-          />
-          <source
-            srcSet={require(`@images/reviews/${image}`)}
-            type="image/jpg"
-          />
-          <img
-            src={require(`@images/reviews/${image}`)}
-            loading="lazy"
-            alt={name}
-          />
-        </S.Image>
+        <S.Image src={getImageUrl(photo.url)} loading="lazy" />
         <S.Name>{name}</S.Name>
       </S.User>
       <S.Text>
-        <input type="checkbox" id={`review-${id}`} />
-        <p className="description">{description}</p>
-        <label className="label-more" htmlFor={`review-${id}`}>
+        <input type="checkbox" id={`review-${name}`} />
+        <p className="description">{text}</p>
+        <label className="label-more" htmlFor={`review-${name}`}>
           Ver tudo
         </label>
       </S.Text>
